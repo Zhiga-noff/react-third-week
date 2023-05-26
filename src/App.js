60dcd03b4c91040e84b7624404d99bc4ed505ddc
kirill <zhiganov@mail.ru>
2023-05-26 12:06:43 +0300

    Урок номер 7 "Единый источник истины"

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
-const Product = ({ name, price }) => {
+import { useState } from 'react';
+import style from './App.module.css';
+
+const Counter = ({ value, setValue }) => {
+
   return (
-    <div>
-      {name} - {price}
+    <>
+      <div>{value}</div>
+      <button onClick={() => setValue(++value)}>{value}+1</button>
+    </>
+  );
+};
+
+export const App = () => {
+  const [value, setValue] = useState(0);
+
+  return (
+    <div className={style.app}>
+      <label htmlFor="">Счетчик {value}</label>
+      <Counter value={value} setValue={setValue} />
     </div>
   );
 };
-Product.propTypes = {
-  name: PropTypes.string,
-  price: PropTypes.number,
-};
-
-
