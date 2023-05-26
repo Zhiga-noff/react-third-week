22294161107e17bfe6397ec27eabd2e5d580d018
kirill <zhiganov@mail.ru>
2023-05-26 12:35:18 +0300

    Stateful и Stateless компоненты

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
 import { useState } from 'react';
 import style from './App.module.css';
 
-const Counter = ({ value, setValue }) => {
-
+const AppLayout = ({ a, b, setA, setB, sum }) => {
   return (
-    <>
-      <div>{value}</div>
-      <button onClick={() => setValue(++value)}>{value}+1</button>
-    </>
+    <div className={style.app}>
+      <div>A: {a}</div>
+      <button onClick={() => setA(a + 1)}>Прибавить 1 к А</button>
+      <div>B: {b}</div>
+      <button onClick={() => setB(b + 1)}>Прибавить 1 к B</button>
+      <div>Сумма A+B: {sum}</div>
+    </div>
   );
 };
 
 export const App = () => {
-  const [value, setValue] = useState(0);
+  const [a, setA] = useState(0);
+  const [b, setB] = useState(0);
 
-  return (
-    <div className={style.app}>
-      <label htmlFor="">Счетчик {value}</label>
-      <Counter value={value} setValue={setValue} />
-    </div>
-  );
+  const sum = a + b;
+
+  return <AppLayout a={a} b={b} setA={setA} setB={setB} sum={sum} />;
 };
